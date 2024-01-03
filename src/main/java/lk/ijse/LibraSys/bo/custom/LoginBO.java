6b933db216a01a3e3f77f24a58ebf0772e173d99
yehani <yehanipamunuwa@gmail.com>
2024-01-03 19:06:28 +0530

    BOFactory implement complete

--- src/main/java/lk/ijse/LibraSys/bo/custom/LoginBO.java
+++ src/main/java/lk/ijse/LibraSys/bo/custom/LoginBO.java
@@ -5,6 +5,7 @@ import lk.ijse.LibraSys.bo.SuperBO;
 import java.sql.SQLException;
 
 public interface LoginBO extends SuperBO {
+
     boolean checkCredentials(String sNumber,String  username,String pw) throws SQLException;
 
 }
